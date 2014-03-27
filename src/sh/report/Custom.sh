130f4daba3b9511715e4543c483288b86769a4c0
iPass Open Mobile Portal API <klee@ipass.com>
2014-03-27 15:59:55 -0700

    This example demonstrates how you can download a Custom Report. The downloaded file will be in zip format, which contains the reports for ALL the child companies

---
+++ src/sh/report/Custom.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# The BASEURL in this script points to the Portal.
+BASEURL=https://openmobile.ipass.com
+
+Echo Enter the username 
+read USER
+
+Echo Enter the password for user $USER
+Read -s PASSWORD
+
+Echo Enter the Company ID
+read COMPANY
+
+Echo Enter the month in the format YYYY-MM
+read MONTH
+
+Echo Enter the filename including the extension, e.g. ClientUserVersion.html
+read FILENAME
+
+# Login to the Portal as a user who has access to view Custom Report
+curl –k –c tmpCookies.txt "$BASEURL/moservices/rest/api/login?UserAgent=apiuser&username=$USER&password=$PASSWORD"
+
+# Obtain a zip file containing the specific reports from all my child companies
+curl –k –request GET –b tmpCookies.txt –o $filename.zip "$BASEURL/moservices/rest/api/ipass/$COMPANY/mo/reports/monthly/custom?User-Agent=apiuser&month=$MONTH&filename=$FILENAME"
+
+# Delete the cookies
+rm tmpCookies.txt
